0881375ad42116485d59da686df73f8c01bee4f1
Jeferson <refej@hotmail.com>
2023-05-28 23:47:54 -0300

    Final Commir

--- coworking/models.py
+++ coworking/models.py
@@ -10,12 +10,12 @@ class Rooms(models.Model):
         verbose_name_plural = "Rooms"
 
 class RoomReservation(models.Model):    
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.CharField(max_length=255, null=True, blank=True)
     room = models.ForeignKey(Rooms, on_delete=models.CASCADE)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     def __str__(self):
-        return f"{self.user.username} / {self.room.name}"
+        return f"{self.user} / {self.room.name}"
 
     class Meta:
         verbose_name_plural = "Room Reservation"
